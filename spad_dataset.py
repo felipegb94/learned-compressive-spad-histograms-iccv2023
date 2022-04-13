3ae49a405fa777cdd517040f5436888ed56d54e2
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-04-12 19:16:57 -0700

    rewrite spad dataset module

--- spad_dataset.py
+++ spad_dataset.py
@@ -1,16 +1,15 @@
 # The SPAD data pre-process function
 
 #### Standard Library Imports
-
+import os
 
 #### Library imports
 import numpy as np
+import scipy
 import scipy.io
 import torch
 import torch.utils.data
-import pytorch_lightning as pl
-from torch.utils.data import random_split, DataLoader
-from torchvision import transforms
+# import pytorch_lightning as pl
 
 from IPython.core import debugger
 breakpoint = debugger.set_trace
@@ -19,79 +18,106 @@ breakpoint = debugger.set_trace
 
 
 
-class NYUv2SPADDataModule(pl.LightningDataModule):
-    def __init__(self, datapath, dims=(64,64,1024), batch_size=2, noise_idx=1, output_size=32, disable_rand_crop=False):
-        """__init__
-        :param datapath: path to text file with list of
-                        training files (intensity files)
-        :param noise_idx: the noise index 1 or 2
-        :param output_size: the output size after random crop
-        """
-        super().__init__()
+# class NYUv2SPADDataModule(pl.LightningDataModule):
+#     def __init__(self, datapath, dims=(64,64,1024), batch_size=2, noise_idx=1, output_size=32, disable_rand_crop=False):
+#         """__init__
+#         :param datapath: path to text file with list of
+#                         training files (intensity files)
+#         :param noise_idx: the noise index 1 or 2
+#         :param output_size: the output size after random crop
+#         """
+#         super().__init__()
 
-        self.datapath = datapath
-        self.batch_size = batch_size
+#         self.datapath = datapath
+#         self.batch_size = batch_size
 
-        self.dims = (1,) + dims
+#         self.dims = (1,) + dims
 
-        self.transforms = transforms.Compose([
-                                transforms.ToTensor()
-        ])
+#         self.transforms = transforms.Compose([
+#                                 transforms.ToTensor()
+#         ])
 
 
 class SpadDataset(torch.utils.data.Dataset):
-    def __init__(self, datapath, noise_idx=None, output_size=32, disable_rand_crop=False):
+    def __init__(self, datalist_fpath, noise_idx=None, output_size=32, disable_rand_crop=False):
         """__init__
-        :param datapath: path to text file with list of
-                        training files (intensity files)
-        :param noise_idx: the noise index 1 or 2
+        :param datapath: path to text file with list of spad data files (intensity files)
+        :param noise_idx: the noise index list to include in the dataset (e.g., 1 or 2
         :param output_size: the output size after random crop
         """
+        with open(datalist_fpath) as f: 
+            self.spad_data_fpaths_all = f.read().split()
 
-        with open(datapath) as f:
-            self.intensity_files = f.read().split()
-        self.spad_files = []
-        self.spad_files.extend([intensity.replace('intensity', 'spad')
-                                    .replace('.mat', '_p{}.mat'.format(noise_idx))
-                                    for intensity in self.intensity_files])
+        self.noise_idx = noise_idx
+        self.spad_data_fpaths = []
+        if(noise_idx is None):
+            self.spad_data_fpaths = self.spad_data_fpaths_all
+        else:
+            if(isinstance(noise_idx, int)):
+                noise_idx = [noise_idx]
+            assert(isinstance(noise_idx, list)), "Input noise idx should be an int or list"
+            for fpath in self.spad_data_fpaths_all:
+                for idx in noise_idx:
+                    # If noise_idx in current fpath, add it, and continue to next fpath
+                    if('_p{}'.format(idx) in os.path.basename(fpath)):
+                        self.spad_data_fpaths.append(fpath)
+                        break
+
+        self.spad_data_fpaths =  self.spad_data_fpaths
+        # self.spad_data_fpaths.extend([intensity.replace('intensity', 'spad')
+        #                             .replace('.mat', '_p{}.mat'.format(noise_idx))
+        #                             for intensity in self.intensity_files])
         self.output_size = output_size
         self.disable_rand_crop = disable_rand_crop
 
+        print("SpadDataset with {} files".format(len(self.spad_data_fpaths)))
+
     def __len__(self):
-        return len(self.spad_files)
+        return len(self.spad_data_fpaths)
 
     def tryitem(self, idx):
-        # simulated spad measurements
-        spad = np.asarray(scipy.sparse.csc_matrix.todense(scipy.io.loadmat(
-            self.spad_files[idx])['spad'])).reshape([1, 64, 64, -1]) # 1,64,64,1024
-        spad = np.transpose(spad, (0, 3, 2, 1))
+        '''
+            Try to load the spad data sample.
+            * All normalization and data augmentation happens here.
+        '''
+        # load spad data
+        spad_data = scipy.io.loadmat(self.spad_data_fpaths[idx])
 
         # normalized pulse as GT histogram
-        rates = np.asarray(scipy.io.loadmat(
-            self.spad_files[idx])['rates']).reshape([1, 64, 64, -1])
+        rates = np.asarray(spad_data['rates'])
+        (nr, nc, n_bins) = rates.shape
+        rates = rates[np.newaxis,: ]
         rates = np.transpose(rates, (0, 3, 1, 2))
-        rates = rates / np.sum(rates, axis=1)[None, :, :, :] 
+        rates = rates / (np.sum(rates, axis=-3, keepdims=True) + 1e-8)
 
-        bins = (np.asarray(scipy.io.loadmat(
-            self.spad_files[idx])['bin']).astype(
-                np.float32).reshape([64, 64])-1)[None, :, :] / 1023
+        # simulated spad measurements
+        spad = np.asarray(scipy.sparse.csc_matrix.todense(spad_data['spad']))
+        spad = spad.reshape((nr, nc, n_bins))
+        spad = spad[np.newaxis, :]
+        spad = np.transpose(spad, (0, 3, 2, 1))
 
-        h, w = spad.shape[2:]
+        # # ground truth depths in units of bins
+        bins = np.asarray(spad_data['bin']).astype(np.float32)
+        bins = bins.reshape((nr, nc))
+        bins = bins[np.newaxis, :]
+        bins = (bins - 1) / (n_bins - 1) # make range 0-n_bins-1 instead of 1-n_bins
+
+        (h, w) = (nr, nc)
         new_h = self.output_size
         new_w = self.output_size
 
         if(self.disable_rand_crop):
-            top = 16
-            left = 16        
+            top = 0
+            left = 0        
         else:
             top = np.random.randint(0, h - new_h)
             left = np.random.randint(0, w - new_w)
 
-        rates = rates[:, :, top: top + new_h,
-                      left: left + new_w]
-        spad = spad[:, :, top: top + new_h,
+        rates = rates[..., top:top + new_h
+                        , left:left + new_w]
+        spad = spad[..., top:top + new_h,
                     left: left + new_w]
-        bins = bins[:, top: top + new_h,
+        bins = bins[..., top: top + new_h,
                     left: left + new_w]
 
         rates = torch.from_numpy(rates)
@@ -113,3 +139,48 @@ class SpadDataset(torch.utils.data.Dataset):
 
 
 if __name__=='__main__':
+    import matplotlib.pyplot as plt
+
+    ## Try test dataset
+    datalist_fpath = './datalists/test_middlebury_SimSPADDataset_nr-72_nc-88_nt-1024_tres-98ps_dark-0_psf-0.txt'
+    noise_idx = None
+    spad_dataset = SpadDataset(datalist_fpath, noise_idx=noise_idx)
+
+    ## Load val dataset
+    datalist_fpath = './datalists/val_nyuv2_SimSPADDataset_nr-64_nc-64_nt-1024_tres-80ps_dark-1_psf-1.txt'
+    noise_idx = [1]
+    spad_dataset = SpadDataset(datalist_fpath, noise_idx=noise_idx, output_size=50)
+
+    batch_size = 1
+
+    loader = torch.utils.data.DataLoader(spad_dataset, batch_size=1, shuffle=True, num_workers=0)
+    iter_loader = iter(loader)
+    for i in range(50):
+        spad_sample = iter_loader.next()
+        
+        spad = spad_sample['spad']
+        rates = spad_sample['rates']
+        bins = spad_sample['bins']
+        idx = spad_sample['idx']
+        print(idx)
+
+        spad = spad.cpu().detach().numpy()[0,:].squeeze()
+        rates = rates.cpu().detach().numpy()[0,:].squeeze()
+        bins = bins.cpu().detach().numpy()[0,:].squeeze()
+        # idx = idx.cpu().detach().numpy().squeeze()        
+
+        plt.clf()
+        plt.subplot(2,2,1)
+        plt.imshow(bins)
+        plt.subplot(2,2,2)
+        plt.imshow(np.argmax(spad, axis=0) / spad.shape[0], vmin=bins.min(), vmax=bins.max())
+        plt.subplot(2,2,3)
+        plt.imshow(np.log(1+np.sum(spad, axis=0)))
+        # plt.imshow(spad.cpu().detach().numpy().squeeze()[0,:].sum(axis=0))
+        plt.pause(0.5)
+        # breakpoint()
+
+
+
+
+
