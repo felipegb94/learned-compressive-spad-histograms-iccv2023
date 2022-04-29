cc38f3df19f16ca98cc6c27a3c9781377089b10b
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-04-28 17:39:15 -0700

    add IRf layer

--- layers_parametric1D.py
+++ layers_parametric1D.py
@@ -7,6 +7,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.nn.init as init
 from IPython.core import debugger
 breakpoint = debugger.set_trace
@@ -39,16 +40,65 @@ class Gaussian1DLayer(nn.Module):
 		out_gaussian = self.a*torch.exp(-0.5*torch.square(loc_reshaped / self.sigma))
 		return out_gaussian
 
+class IRF1DLayer(nn.Module):
+	'''
+		Convolve/Smooth the specified dimension with a 1D filter that is pre-specified
+		The dimension can either be 0, 1, or 2
+		If you want the convolution to happen along the first dimension of your signal then set dim=0
+		If you want the convolution to happen along the last dimension of your signal then set dim=0
+	
+		Input dimensions: (B, 1, D0, D1, D2)
+			- We assume there is only 1 channel
+			- If conv_dim == 0, then convolution is done on D0 dimension
+			- If conv_dim == 1, then convolution is done on D1 dimension
+			- If conv_dim == 2, then convolution is done on D2 dimension
+	'''
+	def __init__(self, irf, conv_dim=0):
+		super(IRF1DLayer, self).__init__()
+
+		assert(irf.ndim==1), "Input IRF should have only 1 dimension"
+
+		self.n = irf.size # Number of bins in irf
+		if(conv_dim == 0): 
+			self.kernel_dims = (self.n, 1, 1)
+			self.conv_padding = (0, 0) + (0, 0) + (self.n//2, self.n//2)
+		elif(conv_dim == 1): 
+			self.kernel_dims = (1, self.n, 1)
+			self.conv_padding = (0, 0) + (self.n//2, self.n//2) + (0, 0)
+		elif(conv_dim == 2): 
+			self.kernel_dims = (1, 1, self.n)
+			self.conv_padding = (self.n//2, self.n//2) + (0, 0) + (0, 0)
+		else: assert(False), "Invalid input conv_dim. conv_dim should be one of 0, 1, 2"
+
+		# Normalize
+		irf = irf / irf.sum()
+		# Create a 3D convolutional filter that will effectively only operate on a single dimension
+		# The filter will have the in/out channel dimensions as 1
+		# The last 3 dimensions are the signal dimension
+		irf_tensor = torch.from_numpy(irf.reshape((1,1)+self.kernel_dims).astype(np.float32))
+		self.irf_weights = torch.nn.Parameter(irf_tensor, requires_grad=False)
+
+	def forward(self, inputs):
+		'''
+			Inputs should have dims (B, 1, D0, D1, D2)
+		'''
+		# Pad inputs appropriately so that the convolution is the same as a circular convolution
+		padded_input = F.pad(inputs, pad=self.conv_padding, mode='circular')
+		# Apply convolution
+		# No need to pad here since we padded above to make sure that the out has the same dimension
+		out = F.conv3d(padded_input, self.irf_weights, stride=1, padding=0)
+		return out
+
 
 if __name__=='__main__':
 	import matplotlib.pyplot as plt
 	from model_utils import count_parameters
+	from research_utils.signalproc_ops import gaussian_pulse
 
 	# Set random input
 	batch_size = 3
 	(nr, nc, nt) = (32, 32, 1024) 
-	in_ch  = 2
-	inputs2D = torch.rand((batch_size, in_ch, nr, nc))
+	inputs2D = torch.rand((batch_size, 1, nr, nc))
 
 	# Eval Network Components
 	inputs = inputs2D
@@ -58,9 +108,21 @@ if __name__=='__main__':
 	print("    input shape: {}".format(inputs.shape))
 	print("    output shape: {}".format(outputs.shape))
 
+	# Generate Gaussian Pulse
+	pulse_len = 25
+	pulse_domain = np.arange(0, pulse_len*5)
+	pulse = gaussian_pulse(pulse_domain, mu=pulse_domain[-1]//2, width=pulse_len, circ_shifted=True)
+	irf_layer = IRF1DLayer(irf=pulse, conv_dim=0)
+	outputs_irf_layer = irf_layer(outputs)
+
 	outputs_np = outputs.cpu().numpy()
+	outputs_irf_layer_np = outputs_irf_layer.cpu().numpy()
 	plt.clf()
 	(r,c) = (np.random.randint(0,nr), np.random.randint(0,nc))
 	plt.plot(outputs_np[0,0, :, r, c])
+	plt.plot(pulse)
+	plt.plot(outputs_irf_layer_np[0,0,:,r,c])
 	plt.title("Row: {}, Col: {}".format(r,c))
+
+
 
