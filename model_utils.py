38c04e5e8915eaa2e38b4531865c18423bf7644a
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-05-03 18:29:50 -0700

    finish removing old model code

--- model_utils.py
+++ model_utils.py
@@ -12,9 +12,8 @@ breakpoint = debugger.set_trace
 from model_ddfn_64_B10_CGNL_ori import LITDeepBoosting, LITPlainDeepBoosting
 from model_ddfn_64_B10_CGNL_ori_depth2depth import LITDeepBoostingDepth2Depth, LITPlainDeepBoostingDepth2Depth
 from model_ddfn_64_B10_CGNL_ori_compressive import LITDeepBoostingCompressive
-from model_ddfn2D_depth2depth import LITPlainDeepBoosting2DDepth2Depth, LITPlainDeepBoosting2DDepth2Depth01Inputs
+from model_ddfn2D_depth2depth import LITPlainDeepBoosting2DDepth2Depth01Inputs
 from model_ddfn2D_depth2depth2hist import LITPlainDeepBoosting2DDepth2Depth2Hist01Inputs
-from model_ddfn2D_depth2hist import LITPlainDeepBoosting2DDepth2Hist01Inputs
 
 
 def count_parameters(model):
@@ -57,28 +56,8 @@ def init_model_from_id(cfg, irf=None):
 						, in_channels = cfg.model.model_params.in_channels
 						, k = cfg.model.model_params.k
 						)
-	elif(cfg.model.model_id == 'DDFN2D_Depth2Depth'):
-		lit_model = LITPlainDeepBoosting2DDepth2Depth(
-						init_lr = cfg.train_params.lri
-						, lr_decay_gamma = cfg.train_params.lr_decay_gamma
-						, p_tv = cfg.train_params.p_tv
-						, in_channels = cfg.model.model_params.in_channels
-						, outchannel_MS = cfg.model.model_params.outchannel_MS
-						, n_ddfn_blocks = cfg.model.model_params.n_ddfn_blocks
-						, num_bins = cfg.dataset.nt
-						)
 	elif(cfg.model.model_id == 'DDFN2D_Depth2Depth_01Inputs'):
 		lit_model = LITPlainDeepBoosting2DDepth2Depth01Inputs(
-						init_lr = cfg.train_params.lri
-						, lr_decay_gamma = cfg.train_params.lr_decay_gamma
-						, p_tv = cfg.train_params.p_tv
-						, in_channels = cfg.model.model_params.in_channels
-						, outchannel_MS = cfg.model.model_params.outchannel_MS
-						, n_ddfn_blocks = cfg.model.model_params.n_ddfn_blocks
-						, num_bins = cfg.dataset.nt
-						)
-	elif(cfg.model.model_id == 'DDFN2D_Depth2Hist_01Inputs'):
-		lit_model = LITPlainDeepBoosting2DDepth2Hist01Inputs(
 						init_lr = cfg.train_params.lri
 						, lr_decay_gamma = cfg.train_params.lr_decay_gamma
 						, p_tv = cfg.train_params.p_tv
--- test.py
+++ test.py
@@ -16,7 +16,7 @@ from model_ddfn_64_B10_CGNL_ori import LITDeepBoosting, LITPlainDeepBoosting
 from model_ddfn_64_B10_CGNL_ori_old import LITDeepBoostingOriginal
 from model_ddfn_64_B10_CGNL_ori_depth2depth import LITDeepBoostingDepth2Depth, LITPlainDeepBoostingDepth2Depth
 from model_ddfn_64_B10_CGNL_ori_compressive import LITDeepBoostingCompressive, LITDeepBoostingCompressiveWithBias
-from model_ddfn2D_depth2depth import LITPlainDeepBoosting2DDepth2Depth, LITPlainDeepBoosting2DDepth2Depth01Inputs
+from model_ddfn2D_depth2depth import LITPlainDeepBoosting2DDepth2Depth01Inputs
 from model_ddfn2D_depth2depth2hist import LITPlainDeepBoosting2DDepth2Depth2Hist01Inputs
 from model_utils import count_parameters
 
@@ -80,8 +80,6 @@ def test(cfg):
 		model = LITDeepBoostingOriginal.load_from_checkpoint("checkpoints/"+ckpt_id)
 	elif(cfg.model_name == 'DDFN_C64B10'):
 		model = LITPlainDeepBoosting.load_from_checkpoint("checkpoints/"+ckpt_id)
-	elif(cfg.model_name == 'DDFN2D_Depth2Depth/B-12_MS-8'):
-		model = LITPlainDeepBoosting2DDepth2Depth.load_from_checkpoint("checkpoints/"+ckpt_id)
 	elif(cfg.model_name == 'DDFN2D_Depth2Depth_01Inputs/B-12_MS-8'):
 		model = LITPlainDeepBoosting2DDepth2Depth01Inputs.load_from_checkpoint("checkpoints/"+ckpt_id, strict=False)
 	elif(cfg.model_name == 'DDFN2D_Depth2Depth_01Inputs/B-16_MS-8'):
