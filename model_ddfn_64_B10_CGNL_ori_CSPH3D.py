7bf5fd8438c741eb5ea2cc225f9044116ad4c19b
felipegb94 <fgutierrez3@wisc.edu>
2022-08-05 13:09:25 -0500

    fix missing flag in LIT model

--- model_ddfn_64_B10_CGNL_ori_CSPH3D.py
+++ model_ddfn_64_B10_CGNL_ori_CSPH3D.py
@@ -23,20 +23,18 @@ class LITPlainDeepBoostingCSPH3D(LITPlainDeepBoosting):
 		, nt_blocks=1
 		, tblock_init = 'TruncFourier'
 		, optimize_tdim_codes = False
+		, optimize_codes = True
 		, h_irf = None
 		, encoding_type = 'separable'
 		):
 		# Init parent class
 		super(LITPlainDeepBoostingCSPH3D, self).__init__(init_lr=init_lr,p_tv=p_tv,lr_decay_gamma=lr_decay_gamma,in_channels=in_channels)
-		self.csph3d_layer = CSPH3DLayer(
-					k=k, num_bins=num_bins, 
-					tblock_init=tblock_init, h_irf=h_irf, optimize_tdim_codes=optimize_tdim_codes,
+		self.csph3d_layer = CSPH3DLayer(k=k, num_bins=num_bins, 
+					tblock_init=tblock_init, h_irf=h_irf, optimize_tdim_codes=optimize_tdim_codes, optimize_codes = optimize_codes, 
 					nt_blocks=nt_blocks, spatial_down_factor=spatial_down_factor, 
 					encoding_type=encoding_type
 					)
 					
-
-
 	def forward(self, x):
 		x_hat = self.csph3d_layer(x)
 		# use forward for inference/predictions
