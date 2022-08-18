09a79a5b90ccc202349b708fee759d2d4b3b20b3
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-08-17 17:27:19 -0700

    fix type

--- scripts_train/ablation_best_csph_out_normalization.sh
+++ scripts_train/ablation_best_csph_out_normalization.sh
@@ -14,13 +14,13 @@
 #### Normalization == Linf
 ## [Pending] Run in Euler (FULL and tblock_init=Rand k=512, codes=(1024x4x4) compression 32x)
 python train.py ++experiment=csph3d_good_norm model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=512 ++model.model_params.spatial_down_factor=4 ++model.model_params.nt_blocks=1 ++model.model_params.optimize_tdim_codes=true ++model.model_params.optimize_codes=true ++model.model_params.encoding_type=full ++model.model_params.csph_out_norm=Linf ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4
-## [Pending] Run in Euler (FULL and tblock_init=Rand k=128, codes=(1024x4x4) compression 32x)
+## [Pending] Run in Euler (FULL and tblock_init=Rand k=128, codes=(1024x4x4) compression 128x)
 python train.py ++experiment=csph3d_good_norm model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=128 ++model.model_params.spatial_down_factor=4 ++model.model_params.nt_blocks=1 ++model.model_params.optimize_tdim_codes=true ++model.model_params.optimize_codes=true ++model.model_params.encoding_type=full ++model.model_params.csph_out_norm=Linf ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4
 
 #### Normalization == L2
 ## [Pending] Run in Euler (FULL and tblock_init=Rand k=512, codes=(1024x4x4) compression 32x)
 python train.py ++experiment=csph3d_good_norm model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=512 ++model.model_params.spatial_down_factor=4 ++model.model_params.nt_blocks=1 ++model.model_params.optimize_tdim_codes=true ++model.model_params.optimize_codes=true ++model.model_params.encoding_type=full ++model.model_params.csph_out_norm=L2 ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4
-## [Pending] Run in Euler (FULL and tblock_init=Rand k=128, codes=(1024x4x4) compression 32x)
+## [Pending] Run in Euler (FULL and tblock_init=Rand k=128, codes=(1024x4x4) compression 128x)
 python train.py ++experiment=csph3d_good_norm model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=128 ++model.model_params.spatial_down_factor=4 ++model.model_params.nt_blocks=1 ++model.model_params.optimize_tdim_codes=true ++model.model_params.optimize_codes=true ++model.model_params.encoding_type=full ++model.model_params.csph_out_norm=L2 ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4
 
 #### Normalization == Cmatsize
