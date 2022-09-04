e615bca7d3875886530fd774b7babaad317876b0
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-09-04 11:41:40 -0700

    move file. update linospad plot script

--- plot_scripts/plot_linospad_results.py
+++ plot_scripts/plot_linospad_results.py
@@ -19,13 +19,14 @@ breakpoint = debugger.set_trace
 #### Local imports
 from tof_utils import bin2depth
 from research_utils import plot_utils
-
+from spad_dataset import Lindell2018LinoSpadDataset
 
 if __name__=='__main__':
 
     ## dirpuath where to store the images
-    experiment_name = 'csph3d_good_norm'
-    out_dirpath = os.path.join('./results/week_2022-08-22/linospad_results', experiment_name)
+    experiment_name = 'debug_generalization'
+    out_base_dirpath = './results/week_2022-08-29/linospad_results'
+    out_dirpath = os.path.join(out_base_dirpath, experiment_name)
     os.makedirs(out_dirpath, exist_ok=True)
 
     ## Parameters
@@ -36,10 +37,19 @@ if __name__=='__main__':
     linospad_dataset_id = 'test_lindell2018_linospad_captured_min'
 
     ## scene we want to plot
-    # scene_id = 'elephant'
-    scene_id = 'kitchen'
+    scene_id = 'elephant'
+    # scene_id = 'kitchen'
     # scene_id = 'lamp'
-    
+
+    ## Create dataset object and load raw data for this scene
+    dataset = Lindell2018LinoSpadDataset(datalist_fpath='datalists/{}.txt'.format(linospad_dataset_id))
+    raw_data_sample = dataset.get_item_by_scene_name(scene_id)
+    plt.clf()
+    plt.imshow(raw_data_sample['intensity_img'].cpu().numpy())
+    plot_utils.remove_ticks()
+    plt.title('Intensity Image from Co-located Camera')
+    plot_utils.save_currfig(dirpath = out_base_dirpath, filename=scene_id)
+
     ## add all model dirpaths
     model_dirpaths = []
     
@@ -47,29 +57,29 @@ if __name__=='__main__':
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/debug/DDFN_C64B10/2022-04-25_192521')
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/debug/DDFN_C64B10_Depth2Depth/loss-kldiv_tv-0.0/2022-05-07_171658')
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/debug/DDFN_C64B10_Depth2Depth/loss-kldiv_tv-1e-5/2022-05-02_085659')
-    
-    ## Add CSPH3D Models (LinfGlobal, TV=[ 1e-5, 3e-6, 1e-6]
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-LinfGlobal/loss-kldiv_tv-1e-05/run-complete_2022-08-23_231951')
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-LinfGlobal/loss-kldiv_tv-1e-06/run-latest_2022-08-24_145125')
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-LinfGlobal/loss-kldiv_tv-3e-06/run-complete_2022-08-26_040500')
 
-    ## Add previous CSPH3Dv1 Separable Models (No Norm, TV=0.0]
+    # ## Add previous CSPH3Dv1 Separable Models (No Norm, TV=0.0]
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/test_csph3d/DDFN_C64B10_CSPH3Dv1/k512_down4_Mt1_HybridGrayFourier-optCt=True-optC=True_separable/loss-kldiv_tv-0.0/2022-08-10_013956')
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/test_csph3d/DDFN_C64B10_CSPH3Dv1/k128_down4_Mt1_HybridGrayFourier-optCt=True-optC=True_separable/loss-kldiv_tv-0.0/2022-08-10_014115')
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/test_csph3d/DDFN_C64B10_CSPH3Dv1/k128_down4_Mt1_Rand-optCt=True-optC=True_separable/loss-kldiv_tv-0.0/2022-08-11_145813')
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/test_csph3d/DDFN_C64B10_CSPH3Dv1/k512_down4_Mt1_Rand-optCt=True-optC=True_separable/loss-kldiv_tv-0.0/2022-08-11_145814')
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none/loss-kldiv_tv-0.0/run-complete_2022-08-10_162141')
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/test_csph3d/DDFN_C64B10_CSPH3Dv1/k128_down4_Mt1_Rand-optCt=True-optC=True_full/loss-kldiv_tv-0.0/2022-07-31_145411')
+    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/test_csph3d/DDFN_C64B10_CSPH3Dv1/k128_down4_Mt1_Rand-optCt=True-optC=True_separable/loss-kldiv_tv-0.0/2022-08-11_145813')
+    
+    ## CSPH3D Separable Models
+    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_debug_generalization/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_HybridGrayFourier-optCt=False-optC=True_separable_norm-LinfGlobal/loss-kldiv_tv-0.0/run-complete_2022-08-29_195412')
+    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_debug_generalization/DDFN_C64B10_CSPH3Dv2/k128_down4_Mt1_HybridGrayFourier-optCt=False-optC=True_separable_norm-LinfGlobal/loss-kldiv_tv-0.0/run-complete_2022-08-29_195412')
 
     ## Add CSPH3D Models (Different Normalization, TV=0.0)
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none/loss-kldiv_tv-0.0/run-complete_2022-08-10_162141')
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-LinfGlobal/loss-kldiv_tv-0.0/run-latest_2022-08-24_144556')
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none/loss-kldiv_tv-0.0/run-complete_2022-08-10_162141')
+    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-LinfGlobal/loss-kldiv_tv-0.0/run-complete_2022-08-24_144556')
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-Linf/loss-kldiv_tv-0.0/run-latest_2022-08-18_011732')
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-L2/loss-kldiv_tv-0.0/run-latest_2022-08-18_013924')
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/test_csph3d/DDFN_C64B10_CSPH3Dv1/k128_down4_Mt1_Rand-optCt=True-optC=True_full/loss-kldiv_tv-0.0/2022-07-31_145411')
     model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k128_down4_Mt1_Rand-optCt=True-optC=True_full_norm-Linf/loss-kldiv_tv-0.0/run-complete_2022-08-18_011947')
-    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_good_norm/DDFN_C64B10_CSPH3Dv2/k128_down4_Mt1_Rand-optCt=True-optC=True_full_norm-L2/loss-kldiv_tv-0.0/run-latest_2022-08-18_013924')
+
+    ## ZNCC normalization
+    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_debug_generalization/DDFN_C64B10_CSPH3D/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none_irf-False_zn-True_zeromu-True_smoothtdimC-False/loss-kldiv_tv-0.0/run-complete_2022-08-30_125727')
+    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_debug_generalization/DDFN_C64B10_CSPH3D/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-LinfGlobal_irf-False_zn-True_zeromu-True_smoothtdimC-False/loss-kldiv_tv-0.0/run-complete_2022-08-30_232221')
+    model_dirpaths.append('outputs/nyuv2_64x64x1024_80ps/csph3d_debug_generalization/DDFN_C64B10_CSPH3D/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none_irf-False_zn-False_zeromu-True_smoothtdimC-False/loss-kldiv_tv-0.0/run-complete_2022-08-30_125727')
+
     # Generate model ids from dirpath
     model_ids = []
     for model_dir in model_dirpaths:
