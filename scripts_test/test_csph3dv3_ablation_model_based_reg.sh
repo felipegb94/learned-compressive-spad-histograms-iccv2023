45d16a68916394b0911126d4331a25732ff72f45
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-09-10 10:54:01 -0700

    update some rmse

--- scripts_test/test_csph3dv3_ablation_model_based_reg.sh
+++ scripts_test/test_csph3dv3_ablation_model_based_reg.sh
@@ -21,6 +21,7 @@ function exit_if_last_cmd_failed(){
 
 ## Dataset we are testing with
 test_dataset=middlebury
+# test_dataset=nyuv2_min
 # test_dataset=lindell2018_linospad_min
 
 ## Dataset we trained the models with
@@ -32,7 +33,7 @@ train_dataset=nyuv2_64x64x1024_80ps
 model_name=DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none/loss-kldiv_tv-0.0
 experiment_name=csph3d_good_norm
 model_dirpath=outputs/${train_dataset}/${experiment_name}/${model_name}/run-complete_2022-08-10_162141
-ckpt_id=epoch=28-step=98695-avgvalrmse=0.0171 # 0.0191 | 120 images==0.3567 | 128 images==0.395
+ckpt_id=epoch=28-step=98695-avgvalrmse=0.0171 # 0.0191 | 120 images==0.3567 | 128 images==0.395--0.3949
 python test.py dataset='"'$test_dataset'"' ++model_name='"'$model_name'"' ++experiment_name=$experiment_name ++model_dirpath='"'$model_dirpath'"' ++ckpt_id='"'$ckpt_id'"' ++train_dataset='"'$train_dataset'"'
 exit_if_last_cmd_failed
 
@@ -40,7 +41,7 @@ exit_if_last_cmd_failed
 model_name=DDFN_C64B10_CSPH3Dv2/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-LinfGlobal/loss-kldiv_tv-0.0
 experiment_name=csph3d_good_norm
 model_dirpath=outputs/${train_dataset}/${experiment_name}/${model_name}/run-complete_2022-08-24_144556
-ckpt_id=epoch=24-step=84843-avgvalrmse=0.0177 # 0.0226 | 120 images== | 128 images==0.0337
+ckpt_id=epoch=24-step=84843-avgvalrmse=0.0177 # 0.0226 | 120 images== | 128 images==0.0337--0.0332
 python test.py dataset='"'$test_dataset'"' ++model_name='"'$model_name'"' ++experiment_name=$experiment_name ++model_dirpath='"'$model_dirpath'"' ++ckpt_id='"'$ckpt_id'"' ++train_dataset='"'$train_dataset'"'
 exit_if_last_cmd_failed
 
@@ -64,7 +65,7 @@ exit_if_last_cmd_failed
 model_name=DDFN_C64B10_CSPH3D/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none_irf-False_zn-False_zeromu-True_smoothtdimC-False/loss-kldiv_tv-0.0
 experiment_name=csph3d_debug_generalization
 model_dirpath=outputs/${train_dataset}/${experiment_name}/${model_name}/run-complete_2022-08-30_125727
-ckpt_id=epoch=29-step=103889-avgvalrmse=0.0173.ckpt # 0.0183 | 120 images== | 128 images==0.0257
+ckpt_id=epoch=29-step=103889-avgvalrmse=0.0173.ckpt # 0.0183 | 120 images== | 128 images==0.0257 -- 0.0251
 python test.py dataset='"'$test_dataset'"' ++model_name='"'$model_name'"' ++experiment_name=$experiment_name ++model_dirpath='"'$model_dirpath'"' ++ckpt_id='"'$ckpt_id'"' ++train_dataset='"'$train_dataset'"'
 exit_if_last_cmd_failed
 
@@ -72,7 +73,7 @@ exit_if_last_cmd_failed
 model_name=DDFN_C64B10_CSPH3D/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none_irf-False_zn-True_zeromu-True_smoothtdimC-False/loss-kldiv_tv-0.0
 experiment_name=csph3d_debug_generalization
 model_dirpath=outputs/${train_dataset}/${experiment_name}/${model_name}/run-complete_2022-08-30_125727
-ckpt_id=epoch=29-step=103889-avgvalrmse=0.0172 # 0.0178 | 120 images== | 128 images==0.0171
+ckpt_id=epoch=29-step=103889-avgvalrmse=0.0172 # 0.0178 | 120 images== | 128 images==0.0171 -- 0.01644
 python test.py dataset='"'$test_dataset'"' ++model_name='"'$model_name'"' ++experiment_name=$experiment_name ++model_dirpath='"'$model_dirpath'"' ++ckpt_id='"'$ckpt_id'"' ++train_dataset='"'$train_dataset'"'
 exit_if_last_cmd_failed
 
@@ -80,7 +81,7 @@ exit_if_last_cmd_failed
 model_name=DDFN_C64B10_CSPH3D/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none_irf-True_zn-True_zeromu-True_smoothtdimC-False/loss-kldiv_tv-0.0
 experiment_name=csph3d_debug_generalization
 model_dirpath=outputs/${train_dataset}/${experiment_name}/${model_name}/run-complete_2022-08-31_122537
-ckpt_id=epoch=28-step=100426-avgvalrmse=0.0171 # 0.01732 | 120 images== | 128 images==0.01737
+ckpt_id=epoch=28-step=100426-avgvalrmse=0.0171 # 0.01732 | 120 images== | 128 images==0.01737 -- 0.01676 --> 0.01789 using test set IRF
 python test.py dataset='"'$test_dataset'"' ++model_name='"'$model_name'"' ++experiment_name=$experiment_name ++model_dirpath='"'$model_dirpath'"' ++ckpt_id='"'$ckpt_id'"' ++train_dataset='"'$train_dataset'"'
 exit_if_last_cmd_failed
 
@@ -88,7 +89,6 @@ exit_if_last_cmd_failed
 model_name=DDFN_C64B10_CSPH3D/k512_down4_Mt1_Rand-optCt=True-optC=True_full_norm-none_irf-True_zn-True_zeromu-True_smoothtdimC-True/loss-kldiv_tv-0.0
 experiment_name=csph3d_debug_generalization
 model_dirpath=outputs/${train_dataset}/${experiment_name}/${model_name}/run-complete_2022-08-30_232221
-# ckpt_id=epoch=23-step=81380-avgvalrmse=0.0156 # 0.0163 | 120 images== | 128 images==0.0156
-ckpt_id=epoch=29-step=102158-avgvalrmse=0.0153 #  0.01601 | 120 images== | 128 images==0.0152
+ckpt_id=epoch=29-step=102158-avgvalrmse=0.0153 #  0.01601 | 120 images== | 128 images==0.0152 -- 0.0145 --> 0.0157 using test set iRF
 python test.py dataset='"'$test_dataset'"' ++model_name='"'$model_name'"' ++experiment_name=$experiment_name ++model_dirpath='"'$model_dirpath'"' ++ckpt_id='"'$ckpt_id'"' ++train_dataset='"'$train_dataset'"'
 exit_if_last_cmd_failed
