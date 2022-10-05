e9171d7413a563475f5ef38574ca198448daa22c
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-10-05 10:27:05 -0700

    update train model status

--- scripts_train/models_csph3d_full_tdim-1024_init-rand.sh
+++ scripts_train/models_csph3d_full_tdim-1024_init-rand.sh
@@ -13,8 +13,8 @@
 
 ## Models with 1024x4x4 Encoding Kernel
 
-# # [STATUS==DONE RAN LOCALLY] 3D CSPH 1024x4x4 - Compression=32x --> k=512
-# python train.py ++experiment=csph3d_models model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=512 ++model.model_params.spatial_down_factor=4 ++model.model_params.nt_blocks=1 ++model.model_params.optimize_codes=true ++model.model_params.optimize_tdim_codes=true ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=full ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
+# [STATUS==DONE RAN LOCALLY] 3D CSPH 1024x4x4 - Compression=32x --> k=512
+python train.py ++experiment=csph3d_models model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=512 ++model.model_params.spatial_down_factor=4 ++model.model_params.nt_blocks=1 ++model.model_params.optimize_codes=true ++model.model_params.optimize_tdim_codes=true ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=full ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
 # [STATUS==DONE RAN EULER] 3D CSPH 1024x4x4 - Compression=64x --> k=256
 python train.py ++experiment=csph3d_models model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=256 ++model.model_params.spatial_down_factor=4 ++model.model_params.nt_blocks=1 ++model.model_params.optimize_codes=true ++model.model_params.optimize_tdim_codes=true ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=full ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
 # [STATUS==DONE RAN EULER] 3D CSPH 1024x4x4 - Compression=128x --> k=128
