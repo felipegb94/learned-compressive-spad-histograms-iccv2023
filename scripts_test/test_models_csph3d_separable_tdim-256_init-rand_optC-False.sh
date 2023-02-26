1f2d7cf53f97d51d50128102d61168f932fe4d46
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2023-02-26 13:23:07 -0600

    add results for csph1d 256x1x1

--- scripts_test/test_models_csph3d_separable_tdim-256_init-rand_optC-False.sh
+++ scripts_test/test_models_csph3d_separable_tdim-256_init-rand_optC-False.sh
@@ -16,19 +16,19 @@ train_dataset=nyuv2_64x64x1024_80ps
 # Models with 256x4x4 Encoding Kernel (THESE MODELS WERE FROM test_models_csph3d_separable_tdim-varying_init-rand)
 ## 3D CSPH 256x4x4 - Compression=32x --> k=128
 model_name=DDFN_C64B10_CSPH3D/k128_down4_Mt4_Rand-optCt=False-optC=False_separable_norm-none_irf-False_zn-True_zeromu-True_smoothtdimC-False/loss-kldiv_tv-0.0
-experiment_name=csph3d_models
+experiment_name=csph3d_models_20230218
 model_dirpath=outputs/${train_dataset}/${experiment_name}/${model_name}/run-complete_2023-02-19_010306
-ckpt_id=epoch=28-step=100424-avgvalrmse=0.0319.ckpt #  128 images== 
+ckpt_id=epoch=28-step=100424-avgvalrmse=0.0319.ckpt #  128 images==0.06622
 python test.py dataset='"'$test_dataset'"' ++model_name='"'$model_name'"' ++experiment_name=$experiment_name ++model_dirpath='"'$model_dirpath'"' ++ckpt_id='"'$ckpt_id'"' ++train_dataset='"'$train_dataset'"'
 ## 3D CSPH 256x4x4 - Compression=64x --> k=64
 model_name=DDFN_C64B10_CSPH3D/k64_down4_Mt4_Rand-optCt=False-optC=False_separable_norm-none_irf-False_zn-True_zeromu-True_smoothtdimC-False/loss-kldiv_tv-0.0
-experiment_name=csph3d_models
+experiment_name=csph3d_models_20230218
 model_dirpath=outputs/${train_dataset}/${experiment_name}/${model_name}/run-complete_2023-02-19_010420
-ckpt_id=epoch=29-step=103887-avgvalrmse=0.0433.ckpt #  128 images== 
+ckpt_id=epoch=29-step=103887-avgvalrmse=0.0433.ckpt #  128 images==0.15015
 python test.py dataset='"'$test_dataset'"' ++model_name='"'$model_name'"' ++experiment_name=$experiment_name ++model_dirpath='"'$model_dirpath'"' ++ckpt_id='"'$ckpt_id'"' ++train_dataset='"'$train_dataset'"'
 ## 3D CSPH 256x4x4 - Compression=128x --> k=32
 model_name=DDFN_C64B10_CSPH3D/k32_down4_Mt4_Rand-optCt=False-optC=False_separable_norm-none_irf-False_zn-True_zeromu-True_smoothtdimC-False/loss-kldiv_tv-0.0
-experiment_name=csph3d_models
+experiment_name=csph3d_models_20230218
 model_dirpath=outputs/${train_dataset}/${experiment_name}/${model_name}/run-complete_2023-02-19_010420
-ckpt_id=epoch=29-step=102157-avgvalrmse=0.0577.ckpt #  128 images== 
+ckpt_id=epoch=29-step=102157-avgvalrmse=0.0577.ckpt #  128 images==0.41653
 python test.py dataset='"'$test_dataset'"' ++model_name='"'$model_name'"' ++experiment_name=$experiment_name ++model_dirpath='"'$model_dirpath'"' ++ckpt_id='"'$ckpt_id'"' ++train_dataset='"'$train_dataset'"'
--- scripts_train/models_csph1d_tdim-256.sh
+++ scripts_train/models_csph1d_tdim-256.sh
@@ -14,29 +14,29 @@
 
 #### Models at k=2 (128x compression)
 
-## [STATUS==PENDING] 1D Temporal CSPH - tblock_init=TruncFourier)
+## [STATUS==DONE RAN IN EULER] 1D Temporal CSPH - tblock_init=TruncFourier)
 python train.py ++experiment=csph3D_tdim-256_20230218 model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=TruncFourier ++model.model_params.k=2 ++model.model_params.spatial_down_factor=1 ++model.model_params.nt_blocks=4 ++model.model_params.optimize_codes=false ++model.model_params.optimize_tdim_codes=false ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=csph1d ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
-## [STATUS==PENDING] 1D Temporal CSPH - tblock_init=Rand)
+## [STATUS==DONE RAN IN EULER] 1D Temporal CSPH - tblock_init=Rand)
 python train.py ++experiment=csph3D_tdim-256_20230218 model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=2 ++model.model_params.spatial_down_factor=1 ++model.model_params.nt_blocks=4 ++model.model_params.optimize_codes=false ++model.model_params.optimize_tdim_codes=false ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=csph1d ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
-# [STATUS==PENDING] 1D Temporal CSPH - tblock_init=Rand + Opt Codes)
+# [STATUS==DONE RAN IN EULER] 1D Temporal CSPH - tblock_init=Rand + Opt Codes)
 python train.py ++experiment=csph3D_tdim-256_20230218 model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=2 ++model.model_params.spatial_down_factor=1 ++model.model_params.nt_blocks=4 ++model.model_params.optimize_codes=true ++model.model_params.optimize_tdim_codes=true ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=csph1d ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
 
 #### Models at k=4 (64x compression)
 
-## [STATUS==PENDING] 1D Temporal CSPH - tblock_init=TruncFourier)
+## [STATUS==DONE RAN IN EULER] 1D Temporal CSPH - tblock_init=TruncFourier)
 python train.py ++experiment=csph3D_tdim-256_20230218 model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=TruncFourier ++model.model_params.k=4 ++model.model_params.spatial_down_factor=1 ++model.model_params.nt_blocks=4 ++model.model_params.optimize_codes=false ++model.model_params.optimize_tdim_codes=false ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=csph1d ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
-## [STATUS==PENDING] 1D Temporal CSPH - tblock_init=Rand)
+## [STATUS==DONE RAN IN EULER] 1D Temporal CSPH - tblock_init=Rand)
 python train.py ++experiment=csph3D_tdim-256_20230218 model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=4 ++model.model_params.spatial_down_factor=1 ++model.model_params.nt_blocks=4 ++model.model_params.optimize_codes=false ++model.model_params.optimize_tdim_codes=false ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=csph1d ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
-# [STATUS==PENDING] 1D Temporal CSPH - tblock_init=Rand + Opt Codes)
+# [STATUS==DONE RAN IN EULER] 1D Temporal CSPH - tblock_init=Rand + Opt Codes)
 python train.py ++experiment=csph3D_tdim-256_20230218 model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=4 ++model.model_params.spatial_down_factor=1 ++model.model_params.nt_blocks=4 ++model.model_params.optimize_codes=true ++model.model_params.optimize_tdim_codes=true ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=csph1d ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
 
 #### Models at k=8 (32x compression)
 
-## [STATUS==PENDING] 1D Temporal CSPH - tblock_init=TruncFourier)
+## [STATUS==DONE RAN IN EULER] 1D Temporal CSPH - tblock_init=TruncFourier)
 python train.py ++experiment=csph3D_tdim-256_20230218 model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=TruncFourier ++model.model_params.k=8 ++model.model_params.spatial_down_factor=1 ++model.model_params.nt_blocks=4 ++model.model_params.optimize_codes=false ++model.model_params.optimize_tdim_codes=false ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=csph1d ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
-## [STATUS==PENDING] 1D Temporal CSPH - tblock_init=Rand)
+## [STATUS==DONE RAN IN EULER] 1D Temporal CSPH - tblock_init=Rand)
 python train.py ++experiment=csph3D_tdim-256_20230218 model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=8 ++model.model_params.spatial_down_factor=1 ++model.model_params.nt_blocks=4 ++model.model_params.optimize_codes=false ++model.model_params.optimize_tdim_codes=false ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=csph1d ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
-# [STATUS==PENDING] 1D Temporal CSPH - tblock_init=Rand + Opt Codes)
+# [STATUS==DONE RAN IN EULER] 1D Temporal CSPH - tblock_init=Rand + Opt Codes)
 python train.py ++experiment=csph3D_tdim-256_20230218 model=DDFN_C64B10_CSPH3D ++model.model_params.tblock_init=Rand ++model.model_params.k=8 ++model.model_params.spatial_down_factor=1 ++model.model_params.nt_blocks=4 ++model.model_params.optimize_codes=true ++model.model_params.optimize_tdim_codes=true ++model.model_params.zero_mean_tdim_codes=true ++model.model_params.apply_zncc_norm=true ++model.model_params.encoding_type=csph1d ++train_params.p_tv=0.0 ++train_params.epoch=30 ++train_params.lri=1e-3 ++random_seed=1235 ++train_params.batch_size=4 ++resume_train=true
 
 
